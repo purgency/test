40233efbbd05b1c5554711151aa13e00b6bbe5cf
U-DESKTOP-VEMDLHR\PhilippQuach <quach.philipp@googlemail.com>
2018-11-22 15:46:28 +0100

    sss

--- timempi.c
+++ timempi.c
@@ -7,7 +7,7 @@ int main (int argc, char *argv[])
     MPI_Init( &argc, &argv );
     MPI_Comm_rank( MPI_COMM_WORLD, &rank );
     MPI_Comm_size( MPI_COMM_WORLD, &size );
-    printf(gethostname() + " : " + gettimeofday() + "from process %d of %d\n",
+    printf(" : from process %d of %d\n",
     rank, size );
     MPI_Finalize();
     return 0;
